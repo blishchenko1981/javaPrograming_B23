be0eea0b2ad583417677c9bd33a3b95e71fea949
blishchenko1981 <blishchenko1981@gmail.com>
2021-07-28 22:27:52 -0400

    checking new laptop

---
+++ officeHours/Wrapper_class.java
@@ -0,0 +1,27 @@
+public class Wrapper_class {
+    public static void main(String[] args) {
+
+        int b = 30;
+        Integer a=b;// autoboxing
+        Integer num1 = Integer.valueOf(300);
+        int num2 = num1;
+
+
+        Integer num = new Integer(300);
+
+
+    }
+}
+/*- Wrapper Class
+            Intro
+
+            - parseInt(),parseDouble() etc vs valueOf()
+            - Auto-Boxing vs Unboxing
+            - WrapperClassMethods
+
+            Task 1:
+                    String str = "(((W&e**_lco73me %T%o Co!@$,,<r>e <J>>>$@av^453a";
+
+                    Write a method that returns : Welcome To Core Java
+
+         */
