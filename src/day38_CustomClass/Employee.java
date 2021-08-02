0d763167e531db070f9c27571864e9cf9429d095
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-02 12:41:14 -0400

    create emplooyee methods toString and setInfo

---
+++ src/day38_CustomClass/Employee.java
@@ -0,0 +1,27 @@
+package day38_CustomClass;
+
+public class Employee {
+    public String name ;
+    public char gender;
+    public int ID;
+    public String jobTitle;
+    public double salary;
+
+    public void setInfo (String name, char gender, int ID, String jobTitle, double salary) {
+        this.name = name;
+        this.gender = gender;
+        this.ID = ID;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", ID=" + ID +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
