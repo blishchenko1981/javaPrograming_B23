16c37c02b64629c2238d458237b4b0b7505c1a28
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-02 11:03:04 -0400

    warmUp rectangle

---
+++ src/day38_CustomClass/Rectangle.java
@@ -0,0 +1,27 @@
+package day38_CustomClass;
+
+public class Rectangle {
+
+    public double length, width;
+
+    public void setInfo(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    public void getInfo(){
+        System.out.println("length = " + length);
+        System.out.println("width = " + width);
+        System.out.println("Area = " + calculateArea());
+        System.out.println("Perimeter = " + calculatePerimeter());
+
+    }
+}
---
+++ src/day38_CustomClass/RectangleObjects.java
@@ -0,0 +1,10 @@
+package day38_CustomClass;
+
+public class RectangleObjects {
+    public static void main(String[] args) {
+        Rectangle rec1 = new Rectangle();
+
+        rec1.setInfo(20.0, 30.0);
+        rec1.getInfo();
+    }
+}
