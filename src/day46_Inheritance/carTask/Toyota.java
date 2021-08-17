af28d7e7c98943d672fa180704dc812895c0fac8
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-17 10:39:20 -0400

    OOP Inheritance,toyota

---
+++ src/day46_Inheritance/carTask/Toyota.java
@@ -0,0 +1,16 @@
+package day46_Inheritance.carTask;
+
+public class Toyota extends Car{
+
+    public static boolean isReliable = true;
+
+    public Toyota(String brand, String model, int year, double price, String color, long miles) {
+        super(brand, model, year, price, color, miles);
+    }
+
+
+
+
+
+
+}
