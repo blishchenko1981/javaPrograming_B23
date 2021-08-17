dce71ad715df3678e8e40f27cd638b55c9c1eaa5
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-17 14:59:06 -0400

    OOP Inheritance, @override

--- src/day46_Inheritance/carTask/Carshop.java
+++ src/day46_Inheritance/carTask/Carshop.java
@@ -1,5 +1,21 @@
 package day46_Inheritance.carTask;
 
 public class Carshop {
+    public static void main(String[] args) {
 
+        Tesla tesla = new Tesla("S", 2020, 35000, "White", 25000);
+        Toyota toyota= new Toyota("Camry", 2019, 27000, "Blue", 45000);
+        BMW bmw = new BMW("x5", 2021, 56000, "Black", 75000);
+
+        toyota.start();
+        tesla.start();
+        bmw.start();
+
+        System.out.println(toyota);
+        System.out.println(tesla);
+        System.out.println(bmw);
+
+
+
+    }
 }
