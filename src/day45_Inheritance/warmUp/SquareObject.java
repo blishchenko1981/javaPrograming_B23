64cd0ac33e7e39443403068c4244d15e5fd221b3
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-16 22:55:24 -0400

    OOP Inheritance,

--- src/day45_Inheritance/warmUp/SquareObject.java
+++ src/day45_Inheritance/warmUp/SquareObject.java
@@ -8,24 +8,30 @@ public class SquareObject {
         Square square = new Square(12);
         Square square1 = new Square(11);
 
-        System.out.println(square.perimeter() + " -perimeter");
-        System.out.println(square.area()+ " -area ");
         System.out.println(square);
+        System.out.println(square1);
+
+        System.out.println("---------------------------------------------");
+
+
 
         square.setSide(4);
-        square.setSide(14);square.setSide(40);
+        square.setSide(14);
+        square.setSide(40);
+
         square1.setSide(12.2);
         square1.setSide(15);
         square1.setSide(18);
         square1.setSide(25);
 
 
-
+        System.out.println(square);
         System.out.println(square1);
 
         boolean equal = square.equal(square1);
         System.out.println(equal + " equal squares");
 
-
+// System.out.println(square.perimeter() + " -perimeter");
+        // System.out.println(square.area()+ " -area ");
     }
 }
