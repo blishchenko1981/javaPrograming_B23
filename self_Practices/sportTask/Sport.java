809ec91fbf6bae1a2af5630e7e33b73af6e00b0d
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-25 12:12:11 -0400

    Sport task is done

--- self_Practices/sportTask/Sport.java
+++ self_Practices/sportTask/Sport.java
@@ -1,13 +1,44 @@
 package sportTask;
 
 public class Sport {
+    private String name;
     private int numberOfPlayers;
     private int numberOfRefere;
     private String rules;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 
-    public Sport (int numberOfPlayers, int numberOfRefere, String rules){
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
+
+    public void setNumberOfPlayers(int numberOfPlayers) {
         this.numberOfPlayers = numberOfPlayers;
+    }
+
+    public int getNumberOfRefere(){
+        return numberOfRefere;
+    }
+
+    public void setNumberOfRefere(int numberOfRefere){
         this.numberOfRefere = numberOfRefere;
+    }
+
+    public String getRules(){
+        return rules;
+    }
+    public void setRules(String rules){
+        this.rules = rules;
+    }
+    public Sport (String name, int numberOfPlayers, int numberOfRefere, String rules){
+        setName(name);
+        setNumberOfRefere(numberOfRefere);
+        setNumberOfPlayers(numberOfPlayers);
+        setRules(rules);
         play();
         toString();
 
@@ -17,9 +48,10 @@ public class Sport {
     }
 
     public String toString(){
-        return  "Sport{"+
-            "Number of players"+ numberOfPlayers+
-                    "Number of referee" + numberOfRefere + "}";
+        return  "Sport{ "+ name+
+            " //Number of players "+ numberOfPlayers+
+                    " //Number of referee " + numberOfRefere +
+                " //rules: " + rules+" }";
 
         }
     }
