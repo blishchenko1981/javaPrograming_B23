09f1e614eab79b037bccf4e1bcbb613eb79af5f2
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-25 13:10:33 -0400

    Planet task is done

---
+++ self_Practices/planet/Earth.java
@@ -0,0 +1,10 @@
+package planet;
+
+public class Earth extends Planet {
+
+    public Earth(String name, double mass, double radius, double surfaceGravity,
+                 double surfaceArea, double volume, double population) {
+        super(name, mass, radius, surfaceGravity, surfaceArea, volume, population);
+    }
+
+}
---
+++ self_Practices/planet/Moon.java
@@ -0,0 +1,7 @@
+package planet;
+
+public class Moon extends Planet{
+    public Moon(String name, double mass, double radius, double surfaceGravity, double surfaceArea, double volume, double population) {
+        super(name, mass, radius, surfaceGravity, surfaceArea, volume, population);
+    }
+}
