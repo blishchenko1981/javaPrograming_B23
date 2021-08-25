f8e63df22a069c2d05bd1ad2f7243f12ffa233f7
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-25 11:00:58 -0400

    sport class done

--- self_Practices/sportTask/Sport.java
+++ self_Practices/sportTask/Sport.java
@@ -1,7 +1,32 @@
 package sportTask;
 
 public class Sport {
+    private int numberOfPlayers;
+    private int numberOfRefere;
+    private String rules;
+
+    public Sport (int numberOfPlayers, int numberOfRefere, String rules){
+        this.numberOfPlayers = numberOfPlayers;
+        this.numberOfRefere = numberOfRefere;
+        play();
+        toString();
+
+    }
+    public void play(){
+        System.out.println("sport is good for everyone");
+    }
+
+    public String toString(){
+        return  "Sport{"+
+            "Number of players"+ numberOfPlayers+
+                    "Number of referee" + numberOfRefere + "}";
+
+        }
+    }
 
 
 
-}
+
+
+
+
